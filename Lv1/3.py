bd24dd1d7e650169e6a921f8997a8f7903b24eb0
Jieun Park <62520045+krapeun@users.noreply.github.com>
2022-04-05 14:01:53 +0900

    Create 3.py

---
+++ Lv1/3.py
@@ -0,0 +1,15 @@
+# 두 정수 사이의 합
+def solution(a, b):
+    answer = 0
+    
+    if a < b:
+        start = a
+        fin = b + 1
+    else:
+        start = b
+        fin = a + 1
+    
+    for i in range(start, fin):
+        answer += i
+    
+    return answer
