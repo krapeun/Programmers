c30224a513d9002d7affb4b0385c5ec9b10da07c
Jieun Park <62520045+krapeun@users.noreply.github.com>
2022-05-12 16:23:16 +0900

    Create StrangeStr.py

---
+++ Lv1/StrangeStr.py
@@ -0,0 +1,17 @@
+# 이상한 문자열 만들기
+
+def solution(s):
+    answer = ''
+    
+    s = list(s.split(' '))
+    
+    for word in s:
+        for i in range(0, len(word)):
+            if i % 2 == 0:
+                answer += word[i].upper()
+            else:
+                answer += word[i].lower()
+        answer += ' '
+
+    answer = ' '.join(answer.split(' ')[0:-1])
+    return answer
