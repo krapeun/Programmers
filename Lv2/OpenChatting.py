2b6c09bd59f85abf9b341dc9d7a8c669fe310da9
Jieun Park <62520045+krapeun@users.noreply.github.com>
2022-05-27 09:17:39 +0900

    Create OpenChatting.py

---
+++ Lv2/OpenChatting.py
@@ -0,0 +1,18 @@
+# 오픈 채팅방
+
+def solution(record):
+    answer = []
+    
+    uid = {}
+    
+    for user in record:
+        if user.split(' ')[0] in 'EnterChange':
+            uid[user.split(' ')[1]] = user.split(' ')[2]
+    
+    for user in record:
+        if user.split(' ')[0] == 'Enter':
+            answer.append(uid[user.split(' ')[1]] + '님이 들어왔습니다.')
+        elif user.split(' ')[0] == 'Leave':
+            answer.append(uid[user.split(' ')[1]] + '님이 나갔습니다.')
+    
+    return answer
